a44a9bf1e4c6999aaa5a77418f282eb54a794a8d
Michael Bestas <mkbestas@lineageos.org>
2019-09-25 23:39:28 +0300

    Fix include and copy file paths
    * We sync this repository in a custom location
    
    Change-Id: Id5e28b26a9abb95016c57c32c1afbc21cea5c262

--- nfc_vendor_product.mk
+++ nfc_vendor_product.mk
@@ -13,7 +13,7 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.nfc.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.xml \
     frameworks/native/data/etc/android.hardware.nfc.hce.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hce.xml \
     frameworks/native/data/etc/android.hardware.nfc.hcef.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.nfc.hcef.xml \
-    vendor/nxp/opensource/halimpl/halimpl/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
+    vendor/nxp/opensource/pn5xx/halimpl/halimpl/libnfc-nci.conf:$(TARGET_COPY_OUT_VENDOR)/etc/libnfc-nci.conf
 
 PRODUCT_PACKAGES += $(NQ_VENDOR_NFC)
 endif
