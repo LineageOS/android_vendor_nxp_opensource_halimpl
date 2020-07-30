022b6365ddb0195dd5e18976b9ee93af14d56853
Gaurav Singhal <gsinghal@codeaurora.org>
2020-07-30 22:13:38 +0530

    NFC: Add dynamic config support for lagoon variant
    Add dynamic configuration support in HAL layer
    for lagoon variant platform to pick Nfc configuration
    file at runtime based on hw platform.
    
    Change-Id: I7b80d04ca987aa4558ab13755930176ee528a738

--- SN100x/halimpl/utils/phNxpConfig.cc
+++ SN100x/halimpl/utils/phNxpConfig.cc
@@ -155,6 +155,7 @@ typedef enum
   TARGET_SM7250_M                      = 440, /**< SM7250_M target */
   TARGET_SM6125                        = 394, /**< SM6125 target */
   TARGET_LAGOON                        = 434, /**< LAGOON target */
+  TARGET_SM_LAGOON_H                   = 459, /**< SM_LAGOON_H target */
   TARGET_BENGAL                        = 417, /**< BENGAL target */
   TARGET_SM_BENGAL_H                   = 444, /**< SM_BENGAL_H target */
   TARGET_SMP_BENGAL_H                  = 445, /**< SMP_BENGAL_H target */
@@ -435,6 +436,7 @@ int CNfcConfig::getconfiguration_id (char * config_file)
         case TARGET_SM8150_SDx55:
         case TARGET_SM6125:
         case TARGET_LAGOON:
+        case TARGET_SM_LAGOON_H:
         case TARGET_SM8350:
         case TARGET_SM_MANNAR:
         case TARGET_SM_MANNAR_H:
@@ -507,6 +509,7 @@ int CNfcConfig::getconfiguration_id (char * config_file)
         case TARGET_SM8150_SDx55:
         case TARGET_SM6125:
         case TARGET_LAGOON:
+        case TARGET_SM_LAGOON_H:
         case TARGET_SM8350:
         case TARGET_SM_MANNAR:
         case TARGET_SM_MANNAR_H:
