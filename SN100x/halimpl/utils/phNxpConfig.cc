1a647ef091dd3852cc384d5159ddf52bf183f529
Tapas Dey <tdey@codeaurora.org>
2021-03-15 11:35:21 +0530

    NFC: Add dynamic config support for kodiak variant
    Dynamic configuration support is added in HAL layer
    for kodiak variant platforms to pick Nfc
    configuration file at runtime based on
    hw platform.
    
    Change-Id: I6c791644cfd20fbe346bb732b2a95c8535d10210

--- SN100x/halimpl/utils/phNxpConfig.cc
+++ SN100x/halimpl/utils/phNxpConfig.cc
@@ -172,6 +172,8 @@ typedef enum
   TARGET_SM_CEDROS                     = 450, /**< SM_CEDROS target */
   TARGET_SM_FRASER                     = 476, /**< SM_FRASER target */
   TARGET_SM_KODIAK                     = 475, /**< SM_KODIAK target */
+  TARGET_SMP_KODIAK                    = 499, /**< SMP_KODIAK target */
+  TARGET_FAROE                         = 515, /**< FAROE target */
   TARGET_DEFAULT                       = TARGET_GENERIC, /**< new targets */
   TARGET_INVALID                       = 0xFF
 } TARGETTYPE;
@@ -455,6 +457,8 @@ int CNfcConfig::getconfiguration_id (char * config_file)
         case TARGET_SM_CEDROS:
         case TARGET_SM_FRASER:
         case TARGET_SM_KODIAK:
+        case TARGET_SMP_KODIAK:
+        case TARGET_FAROE:
             config_id = QRD_TYPE_SN100;
             strlcpy(config_file, config_name_qrd_SN100, MAX_DATA_CONFIG_PATH_LEN);
             break;
@@ -538,6 +542,8 @@ int CNfcConfig::getconfiguration_id (char * config_file)
         case TARGET_SM_CEDROS:
         case TARGET_SM_FRASER:
         case TARGET_SM_KODIAK:
+        case TARGET_SMP_KODIAK:
+        case TARGET_FAROE:
             config_id = MTP_TYPE_SN100;
             strlcpy(config_file, config_name_mtp_SN100, MAX_DATA_CONFIG_PATH_LEN);
             break;
